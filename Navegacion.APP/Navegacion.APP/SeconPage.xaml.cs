fd934c3c9c09b3033c298c0a244852fdb5ff805d
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-05-29 09:41:01 -0500

    NvigationPage

---
+++ Navegacion.APP/Navegacion.APP/SeconPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navegacion.APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SeconPage : ContentPage
+    {
+        public SeconPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
